0d6a50b66ada49654108bc2d79636e8e3c1c98e4
ewn2 <erwin.uppal@gmail.com>
2019-04-24 19:12:17 -0700

    Created

---
+++ sched.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <dirent.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <signal.h>
