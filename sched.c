19374e3020360fc7b49be0a9243fd8ca67f6bc42
ewn2 <erwin.uppal@gmail.com>
2019-04-27 17:11:17 -0700

    Created struct for jobs.dat data, created error message in case of file reading error

--- sched.c
+++ sched.c
@@ -13,3 +13,26 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <signal.h>
+
+struct jobs {
+    int jid;
+    int arrival;
+    int duration;
+};
+
+int main() {
+    FILE *jobList;
+    struct jobs input;
+    jobList = fopen ("jobs.dat", "r");
+    if (jobList == NULL) {
+        printf("\nUnable to either read jobs.dat file\n");
+        exit (1);
+    }
+
+//    while(fread(&input, sizeof(struct jobs), 1, jobList)){
+//
+//    }
+    fclose(jobList);
+    return 0;
+    
+}
